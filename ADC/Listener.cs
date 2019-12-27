24b95d9dd71cd6ebbd762a47157b87a6af385fc0
PapaSmurf <test23445@yahoo.de>
2019-12-27 02:57:31 +0100

    sync

--- ADC/Listener.cs
+++ ADC/Listener.cs
@@ -45,29 +45,28 @@ namespace _2C2P.ADC
             Byte[] set = new Byte[5];
             NetworkStream stream = adcClient.GetStream();
             byte[] cmd = new Byte[1];
-            byte[] byteBufferItems = new Byte[131454];
-            byte[] byteBufferSkills = new Byte[150006];
+            byte[] size = new byte[4];
             while (adcClient.Connected)
             {
                 if (stream.DataAvailable)
                 {
-                    stream.Read(cmd, 0, cmd.Length);
-                    byte[] byteBuffer = null;
-                    ImageRegion region=ImageRegion.enumNull;
-                    switch (cmd[0])
-                    {
-                        case (byte)ImageRegion.skills:
-                            byteBuffer = byteBufferSkills;
-                            region = ImageRegion.skills;
-                            break;
-                        case (byte)ImageRegion.items:
-                            byteBuffer = byteBufferItems;
-                            region = ImageRegion.items;
-                            break;
-                    }
+                    stream.Read(cmd, 0, cmd.Length);                    
+                    ImageRegion region = (ImageRegion)cmd[0];
+                    stream.Read(size, 0, size.Length);
+                    int length = 0;
+                    length += ((int)((int)(size[0])) << 8);
+                    length += ((int)((int)(size[1])) << 8);
+                    length += ((int)((int)(size[2])) << 8);
+                    length += (int) (size[3]);
+
+                    byte[] byteBuffer = new byte[length];
                     stream.Read(byteBuffer, 0, byteBuffer.Length);
-                    stack.Enqueue(new ImageContext() {  raw = (byte[])byteBuffer.Clone(),
-                                                        region = region});
+                    stack.Enqueue(new ImageContext()
+                    {
+                        raw = (byteBuffer),
+                        region = region
+                    });
+                    Thread.Sleep(5);
                 }
             }
         }
